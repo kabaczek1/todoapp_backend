665df93805c2b146e7269e34733e6fb7ffe33301
Krystian Nowakowski <krysti.8642@gmail.com>
2022-06-15 15:52:04 +0200

    user validation function

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
-import { connect } from "mongoose";
-import { User } from "../models/user.model";
-import secrets from "../secrets.json";
+import { User, IUser } from "../models/user.model";
+import Joi from "joi";
+import passwordComplexity from "joi-password-complexity";
 
 export async function run() {
     //await connect(secrets.DB);
@@ -8,9 +8,19 @@ export async function run() {
     const user = new User({
         name: "test",
         email: "test@test.test",
+        password: "aaa",
     });
     await user.save();
 
     console.log(user.email);
     return user.email;
 }
+
+export const validate = (data: IUser) => {
+    const schema = Joi.object({
+        name: Joi.string().required().label("Name"),
+        email: Joi.string().email().required().label("Email"),
+        password: passwordComplexity().required().label("Password"),
+    });
+    return schema.validate(data);
+};
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,13 +1,24 @@
 import { Schema, model } from "mongoose";
+import jwt from "jsonwebtoken";
+import secrets from "../secrets.json";
 
-interface IUser {
+export interface IUser {
     name: string;
     email: string;
+    password: string;
 }
 
 const userSchema = new Schema<IUser>({
     name: { type: String, required: true },
     email: { type: String, required: true },
+    password: { type: String, required: true },
 });
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, secrets.JWTPRIVATEKEY, {
+        expiresIn: "7d",
+    });
+    return token;
+};
 
 export const User = model<IUser>("User", userSchema);
