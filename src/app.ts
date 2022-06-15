41941bb01125e67667d1279065c6f79a30629e94
Krystian Nowakowski <krysti.8642@gmail.com>
2022-06-15 14:49:18 +0200

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -9,56 +9,6 @@ app.use(express.json());
 app.use(cors());
 
 routes(app);
-// app.route("/")
-//     .get((req: Request, res: Response) => {
-//         return res.send("GET");
-//     })
-//     .post((req: Request, res: Response) => {
-//         return res.send("POST");
-//     })
-//     .put((req: Request, res: Response) => {
-//         return res.send("PUT");
-//     })
-//     .delete((req: Request, res: Response) => {
-//         return res.send("DELETE");
-//     });
-
-// const middleware_test = (req: Request, res: Response, next: NextFunction) => {
-//     console.log("middleware_test");
-//     // @ts-ignore
-//     req.params.id = "a";
-//     next();
-// };
-
-// const getTask = (
-//     req: Request<{ id: number }, {}, { name: string }>,
-//     res: Response
-// ) => {
-//     console.log(req.body.name);
-//     console.log(req.params.id);
-//     return res.send(req.params.id);
-// };
-
-app.get("/task", (req: Request, res: Response) => {
-    return res.send(`aaa ${secrets.DB}`);
-    //return res.redirect("http://google.com");
-});
-
-// app.get("/api/task/:id", [middleware_test, getTask]);
-
-// app.get("/", (req: Request, res: Response) => {
-//     return res.json({ test: "aaa" });
-//     //return res.redirect("http://google.com");
-// });
-
-// app.post("/", (req: Request, res: Response) => {
-//     console.log(req.body);
-//     return res.sendStatus(200);
-// });
-
-// app.all("/", (req: Request, res: Response) => {
-//     return res.sendStatus(404);
-// });
 
 const port = 3000;
 app.listen(port, () => {
